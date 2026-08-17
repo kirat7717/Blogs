c80205cc2c8f40abb8f93684b74f8849eb9cc81b
kirat7717 <kiratsinghmalwa@gmail.com>
2026-08-17 12:36:05 +0530

    feat(payment): add payment record creation in MongoDB after Stripe PaymentIntent

--- react-blog-app/server/src/controllers/payment.controller.js
+++ react-blog-app/server/src/controllers/payment.controller.js
@@ -1,5 +1,6 @@
 import stripe from "../configs/stripe.config.js";
 import Blog from "../models/blog.model.js";
+import Payment from "../models/payment.model.js";
 import { createPaymentIntentSchema } from "../validation/payment.validation.js";
 
 export const createPayment = async (req, res) => {
@@ -16,12 +17,14 @@ export const createPayment = async (req, res) => {
     const { blogId } = value;
 
     const blog = await Blog.findById(blogId);
+
     if (!blog) {
       return res.status(404).json({
         success: false,
         message: "Blog not found.",
       });
     }
+
     // Check whether blog requires payment
     if (!blog.isPaid) {
       return res.status(400).json({
@@ -29,27 +32,38 @@ export const createPayment = async (req, res) => {
         message: "This blog is free. Payment is not required.",
       });
     }
+
     // Get price from database
     const amount = blog.price * 100;
-    // Create Stripe PaymentIntent
 
+    // Create Stripe PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "inr",
     });
-  return res.status(200).json({
-  success: true,
-  message: "Payment initiated successfully.",
-  data: {
-    clientSecret: paymentIntent.client_secret,
-    paymentIntentId: paymentIntent.id,
-    status: paymentIntent.status,
-    amount: blog.price,
-  },
-});
-  } catch (error) {
-      console.error("Create PaymentIntent Error:", error);
 
+    // Create payment record in MongoDB
+    const payment = await Payment.create({
+      userId: req.user._id,
+      blogId: blog._id,
+      amount: blog.price,
+      currency: "inr",
+      stripePaymentIntentId: paymentIntent.id,
+      status: paymentIntent.status,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Payment initiated successfully.",
+      data: {
+        clientSecret: paymentIntent.client_secret,
+        paymentIntentId: paymentIntent.id,
+        status: paymentIntent.status,
+        amount: blog.price,
+      },
+    });
+  } catch (error) {
+    console.error("Create PaymentIntent Error:", error);
 
     return res.status(500).json({
       success: false,
